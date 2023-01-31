845117578b6a75fb0c4a141da6c690f37332dc16
Mulualem2 <mulualemasfaw2321@gmail.com>
2023-01-31 09:27:20 +0000

    updating task0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """ Basic Flask app """
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    """Basic Flask app"""
-    return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route('/')
+def root():
+    """ basic Flask app """
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
     app.run()
