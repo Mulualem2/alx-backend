0ce5c15863c92f46f2a0e866c30d1cf28141e379
Mulualem2 <mulualemasfaw2321@gmail.com>
2023-01-31 09:23:24 +0000

    task 0

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Basic Flask app"""
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
